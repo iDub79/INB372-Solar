424e22bd04f1dab5a118eb5c26799c1d73e61238
Ian <ian.wright@optusnet.com.au>
2012-08-14 20:53:56 +1000

    Clear 2

--- INB372-Solar/war/js/scripts.js
+++ INB372-Solar/war/js/scripts.js
@@ -5,8 +5,6 @@ $(function() {
     // initialise Google Map features
     initialiseMap();
     initialiseSearch();
-    
-    // test 3
 
     // add jQuery UI styling to buttons
     $("input:submit, input:button, a, button", ".MyTrip").button();
