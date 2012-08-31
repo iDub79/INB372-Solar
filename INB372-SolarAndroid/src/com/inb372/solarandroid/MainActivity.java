56e5f7691a1d8054a20fc0278a8add5acb89b00c
Ikevink <efsgryphon87@hotmail.com>
2012-08-31 14:22:33 +1000

    Getting strings and integers from text boxes

--- INB372-SolarAndroid/src/com/inb372/solarandroid/MainActivity.java
+++ INB372-SolarAndroid/src/com/inb372/solarandroid/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
@@ -19,9 +18,15 @@ public class MainActivity extends Activity {
     
     // check what view matters
     public void submitDetails(View view) {
-    	EditText editText = (EditText)findViewById(R.id.PanelSize);
-    	//get value of all EditTexts and put into JSP
-    	editText.setText("134");
+    	
+	    	Integer panelSize = Integer.parseInt(((EditText)findViewById(R.id.PanelSize)).getText().toString());
+			Integer panelEfficiency = Integer.parseInt(((EditText)findViewById(R.id.PanelEfficiency)).getText().toString());
+			Integer inverterEfficiency = Integer.parseInt(((EditText)findViewById(R.id.InverterEfficiency)).getText().toString());
+			String address = ((EditText)findViewById(R.id.Address)).getText().toString();
+			String orientation = ((EditText)findViewById(R.id.PanelOrientation)).getText().toString();
+			Integer angle = Integer.parseInt(((EditText)findViewById(R.id.PanelAngle)).getText().toString());
+			Integer sunlight = Integer.parseInt(((EditText)findViewById(R.id.SunlightHours)).getText().toString());
+			Integer consumption = Integer.parseInt(((EditText)findViewById(R.id.PowerConsumption)).getText().toString());
     }
 
 
