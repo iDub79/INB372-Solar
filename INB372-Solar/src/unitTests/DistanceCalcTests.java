f2fe736c65d14020bf3f510e07bc297b814c9f20
Naraceaus <Naraceaus@hotmail.com>
2012-10-09 23:53:01 +1000

    Added two tests for DistanceCalc to prove basic functionality works. One tests morayfield and succeeds in ch0osing Brisbane, one is Northam and succeeds in ch0oosing perth.

--- INB372-Solar/src/unitTests/DistanceCalcTests.java
+++ INB372-Solar/src/unitTests/DistanceCalcTests.java
@@ -11,13 +11,22 @@ public class DistanceCalcTests {
 	
 	
 	@Test
-	public void normal() {
+	public void normalCloseBrisban() {
+		DistanceCalc brisClose = new DistanceCalc();
+		float morayfieldLat = -27.105453f;
+		float morayfieldLon = 152.948145f;
+		float[] closestSunTable = brisClose.findClosestStation(morayfieldLat, morayfieldLon);
+		assert(closestSunTable[0] == 3.3f);
 		
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void normalClosePerth() {
+		DistanceCalc brisClose = new DistanceCalc();
+		float northamLat = -31.65453f;
+		float northamLon = 116.670517f;
+		float[] closestSunTable = brisClose.findClosestStation(northamLat, northamLon);
+		assert(closestSunTable[0] == 9.4f);
 	}
 
 }
